17eef5f8f3807e12d7bfd281ce8d58ff18b669db
iArtz <kanitphong@uservices-thailand.com>
2021-05-07 08:59:30 +0700

    Added useState to App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import { useState } from 'react'
+
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import './App.css'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'A',
+      day: 'Feb 5th at 2:30pm',
+    },
+    {
+      id: 2,
+      text: 'B',
+      day: 'Feb 6th at 3:30pm',
+    },
+    {
+      id: 3,
+      text: 'C',
+      day: 'Feb 6th at 4:30pm',
+    },
+  ])
   return (
     <div className='container'>
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   )
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,24 +1,4 @@
-import { useState } from 'react'
-// import PropTypes from 'prop-types'
-
-const Tasks = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'A',
-      day: 'Feb 5th at 2:30pm',
-    },
-    {
-      id: 2,
-      text: 'B',
-      day: 'Feb 6th at 3:30pm',
-    },
-    {
-      id: 3,
-      text: 'C',
-      day: 'Feb 6th at 4:30pm',
-    },
-  ])
+const Tasks = ({ tasks }) => {
   return (
     <div>
       {tasks.map((task) => (
@@ -27,7 +7,5 @@ const Tasks = () => {
     </div>
   )
 }
-
-// Tasks.propTypes = {}
 
 export default Tasks
