80b51e4634bce5534332be9e3a8c8fb5cff035e9
iArtz <kanitphong@uservices-thailand.com>
2021-05-07 08:44:10 +0700

    Added event to components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Button = ({ text, color }) => {
+const Button = ({ text, color, onClick }) => {
   return (
-    <button style={{ backgroundColor: color }} className='btn'>
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className='btn'
+    >
       {text}
     </button>
   )
@@ -17,6 +21,7 @@ Button.defaultProps = {
 Button.propTypes = {
   text: PropTypes.string,
   color: PropTypes.string,
+  onClick: PropTypes.func,
 }
 
 export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,14 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = ({ title }) => {
+  const onClick = (e) => {
+    console.log(e)
+  }
+
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button text='Add' color='green' />
+      <Button text='Add' color='green' onClick={onClick} />
     </header>
   )
 }
