bbd263d642ba7fe247bea5240d7c043e84859650
iArtz <kanitphong@uservices-thailand.com>
2021-05-07 09:18:24 +0700

    Added icons to task component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,13 @@
+import { FaTimes } from 'react-icons/fa'
+
 import PropTypes from 'prop-types'
 
 const Task = ({ task }) => {
   return (
     <div className='task'>
-      <h3>{task.text}</h3>
+      <h3>
+        {task.text} <FaTimes />
+      </h3>
       <p>{task.day}</p>
     </div>
   )
