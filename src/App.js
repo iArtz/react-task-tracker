11707057462d161924c5160e616ade345941f437
iArtz <kanitphong@uservices-thailand.com>
2021-05-07 09:22:52 +0700

    Added Delete Task prop drilling

--- src/App.js
+++ src/App.js
@@ -21,10 +21,16 @@ function App() {
       day: 'Feb 6th at 4:30pm',
     },
   ])
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   )
 }
