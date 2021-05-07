633d933da2d7e1a7b02fcd14a1f6d326d44dc31f
iArtz <kanitphong@uservices-thailand.com>
2021-05-07 15:18:53 +0700

    Toggle reminder on server

--- src/App.js
+++ src/App.js
@@ -27,6 +27,13 @@ function App() {
     return data
   }
 
+  // Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`${__API__}/${id}`)
+    const data = await res.json()
+    return data
+  }
+
   // Add Task
   const addTask = async (task) => {
     const res = await fetch(`${__API__}`, {
@@ -54,10 +61,22 @@ function App() {
   }
 
   // Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+    const res = await fetch(`${__API__}/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(updatedTask),
+    })
+
+    const data = await res.json()
+
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     )
   }
